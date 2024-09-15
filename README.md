cb02415cf113a0a613f49d1d8c2982dc7dd07769
vitor-ozols <108703032+vitor-ozols@users.noreply.github.com>
2024-09-15 17:37:43 +0000

    ajuste parametro chamada api e dockerfile

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ docker build --build-arg OPENAI_API_KEY=$OPENAI_API_KEY -t chat .
 ```
 
 ```
-docker run chat
+docker run -p 8000:8000 chat
 ```
--- app.py
+++ app.py
@@ -8,70 +8,66 @@ from src.charts import make_chart
 import tiktoken
 import logging
 
-# Configuração do logging
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s - %(levelname)s - %(message)s",
                     handlers=[logging.StreamHandler()])
 
-# Inicializa o cliente da API OpenAI
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
-# Carrega o prompt inicial de um arquivo
 with open('prompts/intro.txt', encoding='utf-8', mode='r') as txt:
     prompt = txt.read()
 
-# Definição do limite de tokens para GPT-4 Turbo
+with open('prompts/json_example.txt', encoding='utf-8', mode='r') as txt_json:
+    json_example = txt_json.read()
+
+# controle de tokens
 MAX_TOKENS = 4096 
 RESERVED_TOKENS = 500
 
-# Função para contar o número de tokens
 def count_tokens(messages, model="gpt-4-turbo"):
     encoding = tiktoken.encoding_for_model(model)
     num_tokens = sum(len(encoding.encode(msg["content"])) for msg in messages)
     return num_tokens
 
-# Função para chamar a API e lidar com respostas que não venham em JSON
-def api_message_handler(session, max_attempts=5):
-    attempts = 0
+
+def api_message_handler(session, max_attempts=5, attempts=0):    
     while attempts < max_attempts:
         response = client.chat.completions.create(model="gpt-4-turbo",
-                                                  messages=session)
+                                                  messages=session,
+                                                  response_format={"type": "json_object"})
         logging.info(f"Resposta API: {response}")
         response_content = response.choices[0].message.content
         try:
             json_content = json.loads(response_content)
             return json_content
         except json.JSONDecodeError:
-            session.append({"role": "system", "content": 'RESPOSTA NÃO VEIO EM JSON'})
+            logging.info(f"Resposta API: API NÃO RESPONDEU NO FORMATO ESPERADO")
+            session.append({"role": "system", "content": f'RESPONDA NESTE FORMATO: {json_example}'})
         attempts += 1
-    # Se o limite de tentativas for alcançado
-    return {"message": "Erro: Não foi possível obter uma resposta válida em JSON."}
+    return {"message": "Erro: ERRO DE API."}
 
-# Função para gerar e enviar gráficos
+# func para fazer graficos
 async def send_chart(df, chart_config):
     fig = make_chart(df, **chart_config)
     elements = [cl.Plotly(name="chart", figure=fig, display="inline")]
     await cl.Message(content="Aqui está o gráfico solicitado:", elements=elements).send()
 
-# Função principal para tratar mensagens
+
 @cl.on_message
 async def handle_message(message: cl.Message):
-    # Obter ou inicializar a sessão do usuário
+
     session = cl.user_session.get("history", [])
     if len(session) == 0:
         session.append({"role": "system", "content": prompt})
 
-    # Adiciona a mensagem do usuário à sessão
     session.append({"role": "user", "content": message.content})
 
-    # Remove mensagens antigas para controle de tokens
     while count_tokens(session) > (MAX_TOKENS - RESERVED_TOKENS):
         if len(session) > 1:
             session.pop(1)
         else:
             break
 
-    # Chama a API da OpenAI e processa a resposta
     json_content = api_message_handler(session=session)
 
     assistant_message = json_content['message']
@@ -79,13 +75,13 @@ async def handle_message(message: cl.Message):
     cl.user_session.set("history", session)
     await cl.Message(content=assistant_message).send()
 
-    # Se houver uma query e um gráfico, gerar o gráfico
+    # se houver uma query e um gráfico, gerar o gráfico
     if 'query' in json_content and 'chart' in json_content:
         query = json_content['query'].replace('database.parquet', 'database/*/*/*/*.parquet')
         df = duckdb.sql(query).df()
         await send_chart(df, json_content['chart'])
 
-    # Se houver apenas a query, exibir o dataframe em formato Markdown
+    # se houver apenas a query, mostrar df em MD
     elif 'query' in json_content and 'chart' not in json_content:
         df = duckdb.sql(json_content['query']).df()
         df_md = df.to_markdown()
